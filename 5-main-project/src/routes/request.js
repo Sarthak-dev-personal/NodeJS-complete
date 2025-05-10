419d7eb5361aece934fd92b84f57b0d044c8f1b5
Sarthak Kapoor <sarthakkapoor@Sarthaks-MacBook-Air.local>
2025-05-10 18:42:22 +0530

    Code changes for cross reference schemas and APIs populate

--- 5-main-project/src/routes/request.js
+++ 5-main-project/src/routes/request.js
@@ -50,4 +50,48 @@ requestRouter.post("/request/send/:status/:toUserId", authenticateUser, async(re
     }
 });
 
+/**
+ * Api to accept/reject connection request.
+ * toUserId should be same as the logged in user id.
+ * The value of status params can only be accept/reject.
+ * The current status of the request can only be interested.
+ * Request ID should be valid.
+ * Offcourse, the user should be logged in.
+ */
+requestRouter.post("/request/review/:status/:requestId", authenticateUser, async(request, response) => {
+    try {
+        const status = request.params.status;
+        const requestId = request.params.requestId;
+        const loggedInUser = request.user;
+        const allowedStatusValues = new Set(["accept", "reject"]);
+
+        if (!allowedStatusValues.has(status)) {
+            throw new Error("Invalid Status value");
+        }
+
+        const existingConnectionRequest = await ConnectionRequestModel.findOne({
+            _id: requestId,
+            toUserId: loggedInUser._id,
+            status: "interested", // Only requests with status as interested can be accepted or rejected.
+        });
+
+        if (!existingConnectionRequest) {
+            throw new Error("The requested connection request doesn't exist!!");
+        }
+
+        existingConnectionRequest.status =  `${status}ed`;
+
+        console.log(existingConnectionRequest.status);
+
+        const data = await existingConnectionRequest.save();
+
+        response.json({
+            message: `${loggedInUser.firstName} you've successfully ${status}ed the connection request`,
+            data,
+        });
+    } catch (error) {
+        response.status(400).json({ message: error.message });
+    }
+});
+
 module.exports = requestRouter;
